286e4d86fba17ee519fb6c14dd0bc43a34da0611
Alec Geatches <throwpi@me.com>
2014-01-29 14:20:39 -0800

    Added project-level testing with quickie

--- src/magic_cube/core.clj
+++ src/magic_cube/core.clj
@@ -3,8 +3,6 @@
 (def two-cube [2 2 2 2 2 2 2])
 (def four-cube [3 2 3 2 2 4 2 3 2 3 2 3 2 2 2 2 2 2 2 2 3 3 2 2 2 2 2 3 4 2 2 2 4 2 3 2 2 2 2 2 2 2 2 2 4 2])
 
-(def directions [:x-plus :x-minus :y-plus :y-minus :z-plus :z-minus])
-
 (defn orthogonal-directions [direction]
   (case direction
     (:x-plus :x-minus) [:y-plus :y-minus :z-plus :z-minus]
@@ -22,9 +20,6 @@
       :z-plus [x y (+ z amount)]
       :z-minus [x y (- z amount)])))
 
-(= [0 1 0] (alter-coordinates 0 0 0 :y-plus 1)) ;; Should alter y-plus coordinate
-(= [1 2 3] (alter-coordinates 1 2 5 :z-minus 2)) ;; Should alter z-minus coordinate
-
 (defn valid-cube? [cube moves size]
   (loop [x 0 y 0 z 0 cube cube moves moves been-to {}]
     (if (empty? moves)
@@ -32,9 +27,6 @@
       (if (and (<= 0 x (- size 1)) (<= 0 y (- size 1)) (<= 0 z (- size 1)) (not (contains? been-to [x y z])))
         (let [[xp yp zp] (alter-coordinates x y z (first moves) (- (first cube) 1))]
           (recur xp yp zp (rest cube) (rest moves) (assoc been-to [x y z] true)))))))
-
-(= true (valid-cube? [2 2 2 2] [:y-plus :x-plus :y-minus :z-plus] 2)) ; Simple arrangement should be valid
-(= nil (valid-cube? [2 2 2 2] [:y-plus :x-plus :y-plus :x-plus] 2)) ; Out-of-bounds arrangement shound be invalid
 
 (defn solve
   ([cube size] (partition (count cube) (filter (complement nil?) (flatten (solve cube [:y-plus] size)))))
@@ -44,5 +36,3 @@
        moves
        (for [move (orthogonal-directions (last moves))]
          (solve cube (conj moves move) size))))))
-
-(solve two-cube 2)
--- test/magic_cube/core_test.clj
+++ test/magic_cube/core_test.clj
@@ -2,6 +2,30 @@
   (:require [clojure.test :refer :all]
             [magic-cube.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest orthogonal-directions-test
+  (testing "Orthogonal directions to +x are y and z directions"
+    (is (= [:y-plus :y-minus :z-plus :z-minus] (orthogonal-directions :x-plus)))))
+
+(deftest alter-coodinates-plus
+  (testing "Should alter +y coordinate"
+    (is (= [0 1 0] (alter-coordinates 0 0 0 :y-plus 1)))))
+
+(deftest alter-coodinates-minus
+  (testing "Should alter -z coordinate"
+    (is (= [1 2 3] (alter-coordinates 1 2 5 :z-minus 2)))))
+
+(deftest valid-cube-simple-test
+  (testing "Simple arrangement should be valid"
+    (is (= true (valid-cube? [2 2 2 2] [:y-plus :x-plus :y-minus :z-plus] 2)))))
+
+(deftest valid-cube-out-of-bounds-test
+  (testing "Out-of-bounds arrangement should be valid"
+    (is (= nil (valid-cube? [2 2 2 2] [:y-plus :x-plus :y-plus :x-plus] 2)))))
+
+(deftest solve-very-simple-test
+  (testing "A one-piece puzzle should have one solution"
+    (is (= 1 (count (solve [2] 2))))))
+
+(deftest solve-simple-test
+  (testing "A two-peice puzzle should have four solutions"
+    (is (= 4 (count (solve [2 2] 2))))))

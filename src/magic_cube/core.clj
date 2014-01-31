f134b8233a8f63f0860b0dd19d56f0d1381fb821
Alec Geatches <throwpi@me.com>
2014-01-31 13:27:49 -0800

    Fixed valid-cube? to check all visited positions, rather than just edges of segments

--- src/magic_cube/core.clj
+++ src/magic_cube/core.clj
@@ -4,26 +4,33 @@
   (case direction
     (:x-plus :x-minus) [:y-plus :y-minus :z-plus :z-minus]
     (:y-plus :y-minus) [:x-plus :x-minus :z-plus :z-minus]
-    (:z-plus :z-minus) [:x-plus :x-minus :y-plus :y-minus]
-   ))
+    (:z-plus :z-minus) [:x-plus :x-minus :y-plus :y-minus]))
 
-(defn alter-coordinates [x y z move amount]
-  (if (not (or (nil? move) (nil? amount)))
+(defn alter-coordinates [x y z f move]
+  (if (not (nil? move))
     (case move
-      :x-plus [(+ x amount) y z]
-      :x-minus [(- x amount) y z]
-      :y-plus [x (+ y amount) z]
-      :y-minus [x (- y amount) z]
-      :z-plus [x y (+ z amount)]
-      :z-minus [x y (- z amount)])))
+      (:x-plus :x-minus) [(f x) y z]
+      (:y-plus :y-minus) [x (f y) z]
+      (:z-plus :z-minus) [x y (f z)])))
+
+(defn touched-coordinates [x y z move amount]
+  (let [move-map {:x-plus inc :x-minus dec :y-plus inc :y-minus dec :z-plus inc :z-minus dec}]
+    (loop [x x y y z z touched '() i 0]
+      (let [[x y z] (alter-coordinates x y z (move move-map) move)]
+        (if (= i amount)
+          touched
+          (recur x y z (conj touched (list x y z)) (inc i)))))))
 
 (defn valid-cube? [cube moves size]
-  (loop [x 0 y 0 z 0 cube cube moves moves been-to {}]
-    (if (empty? moves)
-      true
-      (if (and (every? #(<= 0 % (- size 1)) [x y z]) (not (get been-to [x y z])))
-        (let [[xp yp zp] (alter-coordinates x y z (first moves) (- (first cube) 1))]
-          (recur xp yp zp (rest cube) (rest moves) (assoc been-to [x y z] true)))))))
+  (loop [x 0 y 0 z 0 cube cube moves moves touched {(list x y z) true}]
+    (if (every? #(<= 0 % (- size 1)) [x y z])
+      (if (empty? moves)
+        true
+        (let [touching (touched-coordinates x y z (first moves) (- (first cube) 1))
+              [x y z] (first touching)]
+          (if (not (some #(get touched %) touching))
+            (recur x y z (rest cube) (rest moves) (reduce #(assoc %1 %2 true) touched touching))))))))
+
 (defn solve
   ([cube size] (solve cube [:y-plus] size))
   ([cube moves size]
--- test/magic_cube/core_test.clj
+++ test/magic_cube/core_test.clj
@@ -1,21 +1,31 @@
+
 (ns magic-cube.core-test
   (:require [clojure.test :refer :all]
             [magic-cube.core :refer :all]))
 
 (def two-cube [2 2 2 2 2 2 2])
+(def three-cube [3 3 3 2 2 2 2 2 3 3 3 2 3 3 3 2 2])
 (def four-cube [3 2 3 2 2 4 2 3 2 3 2 3 2 2 2 2 2 2 2 2 3 3 2 2 2 2 2 3 4 2 2 2 4 2 3 2 2 2 2 2 2 2 2 2 4 2])
 
 (deftest orthogonal-directions-test
   (testing "Orthogonal directions to +x are y and z directions"
     (is (= [:y-plus :y-minus :z-plus :z-minus] (orthogonal-directions :x-plus)))))
 
-(deftest alter-coodinates-plus
+(deftest alter-coodinates-plus-test
   (testing "Should alter +y coordinate"
-    (is (= [0 1 0] (alter-coordinates 0 0 0 :y-plus 1)))))
+    (is (= [0 1 0] (alter-coordinates 0 0 0 inc :y-plus)))))
 
-(deftest alter-coodinates-minus
+(deftest alter-coodinates-minus-test
   (testing "Should alter -z coordinate"
-    (is (= [1 2 3] (alter-coordinates 1 2 5 :z-minus 2)))))
+    (is (= [1 2 4] (alter-coordinates 1 2 5 dec :z-minus)))))
+
+(deftest touched-coodinates-one-test
+  (testing "Should return one touched coordinate when moving once"
+    (is (= '((0 1 0)) (touched-coordinates 0 0 0 :y-plus 1)))))
+
+(deftest touched-coodinates-two-test
+  (testing "Should return two touched coordinates when moving through two spaces"
+    (is (= '((1 2 1) (1 2 2)) (touched-coordinates 1 2 3 :z-minus 2)))))
 
 (deftest valid-cube-simple-test
   (testing "Simple arrangement should be valid"
@@ -32,3 +42,7 @@
 (deftest solve-simple-test
   (testing "A two-peice cube should have a seven-move solution"
     (is (= 7 (count (solve two-cube 2))))))
+
+(deftest solve-three-cube-test
+  (testing "A 3x3x3 cube should have a 17-move solution"
+    (is (= 17 (count (solve three-cube 3))))))
